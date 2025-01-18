6231d8c4f77896733c7037e35368fba2c9d46140
Tybie <71943949+Tybiee@users.noreply.github.com>
2025-01-18 15:14:00 -0500

    Co-authored-by: theresajp <theresajp@users.noreply.github.com> Co-authored-by: BryBryTheFry <BryBryTheFry@users.noreply.github.com>

--- src/main/java/frc/robot/commands/DriveToTag.java
+++ src/main/java/frc/robot/commands/DriveToTag.java
@@ -8,8 +8,10 @@ import javax.xml.crypto.dsig.Transform;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.MotorConstants;
 import frc.robot.subsystems.Drive;
@@ -19,14 +21,17 @@ public class DriveToTag extends Command {
 
   private Drive _driveSubsystem;
   private Transform3d _tagFieldTransform3d;
-  private PIDController _tagPIDController;
+  private PIDController _tagHeadingPIDController;
+  private double _deltaX, _deltaY;
   
   /** Creates a new DriveToTag. */
-  public DriveToTag(Drive driveSubsystem, Transform3d tagFieldTransform3d) {
+  public DriveToTag(Drive driveSubsystem, Transform3d tagFieldTransform3d, double deltaX, double deltaY) {
     _driveSubsystem = driveSubsystem;
     _tagFieldTransform3d = tagFieldTransform3d;
-    _tagPIDController = MotorConstants.tagHeadingPIDController;
-
+    _tagHeadingPIDController = MotorConstants.tagHeadingPIDController;
+    _deltaX = deltaX;
+    _deltaY = deltaY;
+    
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveSubsystem);
   }
@@ -41,10 +46,14 @@ public class DriveToTag extends Command {
     // TODO: Update transform from vision
 
     // TODO: Add offset for camera position/bumper position/height/etc
-    Transform3d robotFieldTransform3d = _tagFieldTransform3d.inverse();
-
+    Transform3d robotFieldTransform3d = _tagFieldTransform3d.inverse().plus(new Transform3d(_deltaX, _deltaY, 0, new Rotation3d(0.0, 0.0, 0.0)));
     
 
+    double velocityX = 0;//_tagXPIDController.calculate(robotFieldTransform3d.getX());
+    double velocityY = 0;
+    double velocityTheta = _tagHeadingPIDController.calculate(robotFieldTransform3d.getRotation().getAngle());
+    
+    _driveSubsystem.setDesiredRobotRelativeSpeeds(new ChassisSpeeds(velocityX, velocityY, velocityTheta));
 
   }
 
